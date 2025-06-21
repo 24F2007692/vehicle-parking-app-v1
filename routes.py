fc77e052551575addae255d201d062be580e3515
24F2007692 <24f2007692@ds.study.iitm.ac.in>
2025-06-21 23:27:39 +0530

    Milestone-VP Admin-Dashboard-Management Milestone-VP Search-Implemented Milestone-VP Responsive-UI Milestone-VP Flask-Integration

--- routes.py
+++ routes.py
@@ -30,6 +30,10 @@ def dashboard():
         return redirect(url_for('login'))
 
     user = User.query.get(session['user_id'])
+    if not user:
+        session.pop('user_id', None)
+        flash("User not found. Please login again.")
+        return redirect(url_for('login'))
 
     if user.role == 'admin':
         lots = ParkingLot.query.all()
@@ -72,7 +76,7 @@ def dashboard():
                 history.append([res.id, lot.address, res.vehicle_num,
                                res.booking_time, res.Leaving_timestamp, spot.id,])
 
-        return render_template("user_dashboard.html")
+        return render_template("user_dashboard.html", lots=lots, location=location, history=history)
 
 
 # search functionality for ADMIN
@@ -175,14 +179,176 @@ def signup_post():
     return redirect(url_for('dashboard'))
 
 
-@app.route("/search_lot", methods=['POST'])
-def search_lot():
+# ADMIN KE ROUTES
+
+# FETCH USERS
+@app.route('/allusers')
+def allusers():
     if 'user_id' not in session:
         flash('Please login to Continue')
         return redirect(url_for('login'))
-    location = request.form.get('location')
-    if location:
-        return redirect(url_for('dashboard', location=location))
+    users = User.query.all()
+    data = [[user.id, user.email, user.name, user.address, user.pincode]
+            for user in users]
+    return render_template("admin_all_users.html", data=data)
+
+
+# ADD A PARKING LOT
+@app.route('/addlot', methods=['GET', 'POST'])
+def addlot():
+    if 'user_id' not in session:
+        flash('Please login to Continue')
+        return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        location = request.form.get('location')
+        price = request.form.get('price')
+        address = request.form.get('address')
+        pincode = request.form.get('pincode')
+        max_spots = int(request.form.get('max_spots'))
+
+        newlot = ParkingLot(prime_location_name=location, price_per_hr=price,
+                            max_spots=max_spots, address=address, pincode=pincode)
+
+        db.session.add(newlot)
+        db.session.commit()
+        flash("New lot added successfully")
+
+    # add spots in parking lot:
+        for i in range(1, max_spots+1):
+            spot = ParkingSpot(lot_id=newlot.id, spot_no=i, status='A')
+            db.session.add(spot)
+        db.session.commit()
+        return redirect(url_for('dashboard'))
+    return render_template("admin_new_parklot.html")
+
+
+# EDIT A PARKING LOT (also changing spots acc to max_spots)
+@app.route('/edit_lot/<int:lot_id>', methods=['GET', 'POST'])
+def edit_lot(lot_id):
+    if 'user_id' not in session:
+        flash('Please login to Continue')
+        return redirect(url_for('login'))
+
+    lot = ParkingLot.query.filter_by(id=lot_id).first()
+
+    if request.method == 'POST':
+        lot.prime_location_name = request.form.get('location')
+        lot.price_per_hr = request.form.get('price')
+        lot.address = request.form.get('address')
+        lot.pincode = request.form.get('pincode')
+        lot.max_spots = request.form.get('max_spots')
+
+        # edit spots according to updated max_spots:
+        reserved_spots = ParkingSpot.query.filter_by(
+            lot_id=lot_id, status='O').all()
+        for spot in reserved_spots:
+            Reservation.query.filter_by(spot_id=spot.id).delete()
+        ParkingSpot.query.filter_by(lot_id=lot_id).delete()
+
+        for i in range(1, int(lot.max_spots)+1):
+            spot = ParkingSpot(lot_id=lot_id, spot_no=i, status='A')
+            db.session.add(spot)
+
+        db.session.commit()
+        flash("lot Updated successfully")
+        return redirect(url_for('dashboard'))
+
+    return render_template("admin_new_parklot.html", lot=lot)
+
+
+# DELETE A PARKING LOT
+@app.route('/del_lot/<int:lot_id>')
+def del_lot(lot_id):
+    if 'user_id' not in session:
+        flash('Please login to Continue')
+        return redirect(url_for('login'))
+
+    lot = ParkingLot.query.get(lot_id)
+    if not lot:
+        flash('Parking lot not found')
+        return redirect(url_for('dashboard'))
+
+    # delete reservation
+    reserved_spots = ParkingSpot.query.filter_by(
+        lot_id=lot_id, status='O').all()
+    for spot in reserved_spots:
+        Reservation.query.filter_by(spot_id=spot.id).delete()
+
+    # delete spots of this lot
+    ParkingSpot.query.filter_by(lot_id=lot_id).delete()
+
+    # delete lot finally
+    lot = ParkingLot.query.filter_by(id=lot_id).first()
+    db.session.delete(lot)
+
+    db.session.commit()
+    flash("lot deleted successfully")
+    return redirect(url_for('dashboard'))
+
+
+# view parking spot details:
+@app.route("/view_spot/<int:spot_id>", methods=["GET", "POST"])
+def view_spot(spot_id):
+
+    if 'user_id' not in session:
+        flash('Please login to Continue')
+        return redirect(url_for('login'))
+
+    spot = ParkingSpot.query.filter_by(id=spot_id).first()
+    if not spot:
+        flash("Spot not found")
+        return redirect(url_for('dashboard'))
+
+    return render_template("admin_parking_spot.html", status=spot.status, spot_id=spot_id)
+
+# Occupied spot details
+@app.route("/O_spot/<int:spot_id>")
+def O_spot(spot_id):
+    if 'user_id' not in session:
+        flash('Please login to Continue')
+        return redirect(url_for('login'))
+
+    O_spot = Reservation.query.filter_by(spot_id=spot_id).first()
+    spot = ParkingSpot.query.filter_by(id=spot_id).first()
+    lot = ParkingLot.query.filter_by(id=spot.lot_id).first()
+
+    if O_spot:
+        booking_time = O_spot.booking_time.replace(tzinfo=timezone.utc)
+        release_time = datetime.now(timezone.utc)
+        duration = (release_time - booking_time).total_seconds() / 3600
+        duration = max(duration, 0.01)
+        price_per_hr = float(lot.price_per_hr)
+        total_cost = round(duration * price_per_hr, 2)
+        return render_template("admin_O_parking_details.html", O_spot=O_spot, total_cost=total_cost)
+
+    return redirect(url_for('dashboard'))
+
+
+# Delete a parking spot
+@app.route("/del_spot/<int:spot_id>", methods=["GET", "POST"])
+def del_spot(spot_id):
+    if 'user_id' not in session:
+        flash('Please login to Continue')
+        return redirect(url_for('login'))
+
+    spot = ParkingSpot.query.filter_by(id=spot_id).first()
+    if not spot:
+        flash("Spot not found")
+        return redirect(url_for('dashboard'))
+
+    # updating max_spots in ParkingLot table
+    lot = ParkingLot.query.filter_by(id=spot.lot_id).first()
+
+    # reservation bhi delete karni pdegi agar occupied delete kri to
+    Reservation.query.filter_by(spot_id=spot_id).delete()
+
+    db.session.delete(spot)
+    if lot and lot.max_spots > 0:
+        lot.max_spots -= 1
+    db.session.commit()
+
+    flash("Spot deleted successfully")
     return redirect(url_for('dashboard'))
 
 
@@ -194,5 +360,3 @@ def logout():
 
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
